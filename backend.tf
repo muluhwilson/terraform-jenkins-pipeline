d54a32c299031c3c505e212a9337ee35c3c72544
wilson <muluhwilson@yahoo.com>
2021-07-23 05:21:23 -0700

    my terraform jenkin pipeline

--- backend.tf
+++ backend.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  backend "s3" {
+    bucket = "jenkinsterraform123"
+    key = "jt.tfstate"
+    region = "us-west-2"
+    dynamodb_table = "dynamodb1"
+  }
+}
--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,8 @@
+resource "aws_instance" "muluh" {
+  ami = "ami-0dc8f589abe99f538"
+  instance_type = "t2.micro"
 
+  tags = {
+    Name = "jenkins-terraform"
+  }
+}
